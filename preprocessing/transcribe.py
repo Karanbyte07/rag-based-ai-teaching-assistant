47130723363275d365b70aa17ead5e79a3d27486
karanjeet kumar <karanbyte07@gmail.com>
2026-07-14 16:01:38 +0530

    refactor transcription script to remove Azure API dependency and streamline audio processing

--- preprocessing/transcribe.py
+++ preprocessing/transcribe.py
@@ -1,45 +1,41 @@
+import whisper
 import os
 import json
-import requests
-from dotenv import load_dotenv
 
-# load environment variables 
-load_dotenv()
 
-# directories
+# Input and output directories
 audio_dir = "data/audios"
 transcript_dir = "data/transcripts"
 
-# read whisper config from env
-WHISPER_API_KEY = os.getenv("AZURE_WHISPER_API_KEY")
-WHISPER_BASE_URL = os.getenv("AZURE_WHISPER_BASE_URL")
+model = whisper.load_model("base")
 
-# loop through audios
-for audio in os.listdir(audio_dir):
+
+audios = os.listdir(audio_dir)
+
+for audio in audios:
     audio_path = os.path.join(audio_dir, audio)
+    print(f"Transcribing {audio}........")
 
-    with open(audio_path, "rb") as f:
-        files = {"files": f}
-        data = {
-            "language": "hi",
-            "task": "translate"
-        }
-        response = requests.post(
-            f"{WHISPER_BASE_URL}/audio/transcriptions",
-            headers={"api-key": WHISPER_API_KEY},
-            data=data,
-            files=files
-        )
+    
+    #transcribe the audio
+    result = model.transcribe(
+        audio=audio_path,
+        language="hi",
+        task="translate"
+    )
 
-    result = response.json()
+    #output file name
+    output_file = audio.replace(".mp3", ".json")
 
-    # Save transcript with same name as audio (indented to be inside the loop)
-    transcript_filename = os.path.splitext(audio)[0] + ".json"
-    transcript_path = os.path.join(transcript_dir, transcript_filename)
+    #output path
+    output_path = os.path.join(transcript_dir, output_file)
 
-    with open(transcript_path, "w", encoding="utf-8") as f:
+    #save transcript (use UTF-8 to avoid Windows encoding errors)
+    with open(output_path, "w", encoding="utf-8") as f:
         json.dump(result, f, ensure_ascii=False, indent=2)
 
-    print(f"Transcribed and saved: {transcript_filename}")
+    print(f"Saved: {output_file} in {transcript_dir}")
 
 
+
+print("All audio files have been transcribed Successfully!")
