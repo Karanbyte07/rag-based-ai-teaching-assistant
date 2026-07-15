9f9bff6a2f2b4c7bc0a4df04baa631723a80a7b5
karanjeet kumar <karanbyte07@gmail.com>
2026-07-15 14:52:48 +0530

    implement script to parse transcript segments into structured metadata chunks

--- preprocessing/chunking.py
+++ preprocessing/chunking.py
@@ -1,6 +1,8 @@
 import json
 import os
-from transcribe import result
+
+
+
 
 base_dir = os.path.dirname(os.path.dirname(__file__))
 chunk_dir = os.path.join(base_dir, "data", "chunks")
@@ -15,26 +17,33 @@ for audio in audios:
         tutorial_name = audio.split("_")[1][:-4]
     # print(f"tutorial number:->  {tutorial_num}  tutorial name:-> {tutorial_name}" )
 
-
-
-# creating a chunks from the segments
 chunks = []
-for segment in result['segments']:
-    chunks.append({"tutorial_number":tutorial_num,
-                   "tutorial_name":tutorial_name,
-                    "start": segment['start'],
-                    "end": segment['end'], 
-                    "text": segment['text']})
-    chunk_with_metadata = {"chunks": chunks, "text" : result["text"]}
+for transcripts in os.listdir("data/transcripts"):
+    # print(transcript)
+    with open(f"data/transcripts/{transcripts}" , "rb") as f:
+        result = json.load(f)
+    
+    # creating a chunks from the segments
+
+    for segment in result['segments']:
+        chunks.append({"tutorial_number":tutorial_num,
+                    "tutorial_name":tutorial_name,
+                        "start": segment['start'],
+                        "end": segment['end'], 
+                        "text": segment['text']})
+        # chunk_with_metadata = {"chunks": chunks, "text" : result["text"]}
     
 
-# print(chunks_with_metadata)
+
+   
+
+    print(chunks)
 
 
-#saving chunks in data/chunks
-os.makedirs(chunk_dir, exist_ok=True)
-with open(chunk_path, "w") as f:
-    json.dump(chunk_with_metadata, f)
+# #saving chunks in data/chunks
+# os.makedirs(chunk_dir, exist_ok=True)
+# with open(chunk_path, "w") as f:
+#     json.dump(chunk_with_metadata, f)
 
-display_path = os.path.join(os.path.basename(base_dir), "data", "chunks", "chunks.json")
-print(f"Your audio is converted into the texts succesfully!{display_path}")
+# display_path = os.path.join(os.path.basename(base_dir), "data", "chunks", "chunks.json")
+# print(f"Your audio is converted into the texts succesfully!{display_path}")
