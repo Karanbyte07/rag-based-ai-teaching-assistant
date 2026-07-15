f1ae9e543685bcadb06c26c8d5a9b0234a1b12d1
karanjeet kumar <karanbyte07@gmail.com>
2026-07-16 00:25:25 +0530

    add script to preprocess and chunk transcript JSON files with metadata

--- preprocessing/chunking.py
+++ preprocessing/chunking.py
@@ -1,49 +1,44 @@
 import json
 import os
 
-
-
-
 base_dir = os.path.dirname(os.path.dirname(__file__))
+
 chunk_dir = os.path.join(base_dir, "data", "chunks")
-chunk_path = os.path.join(chunk_dir, "chunks.json")
 
-audios = os.listdir("data/audios")
 
-for audio in audios:
-    # print(audio)
-    if("_" in audio):
-        tutorial_num = audio.split("_")[0]
-        tutorial_name = audio.split("_")[1][:-4]
-    # print(f"tutorial number:->  {tutorial_num}  tutorial name:-> {tutorial_name}" )
-
-chunks = []
-for transcripts in os.listdir("data/transcripts"):
-    # print(transcript)
-    with open(f"data/transcripts/{transcripts}" , "rb") as f:
+#loop through every transcript
+for transcript_file in os.listdir("data/transcripts"):
+    with open(f"data/transcripts/{transcript_file}", "r", encoding="utf-8") as f:
         result = json.load(f)
-    
+
+    # Remove .json extension
+    filename = transcript_file.replace(".json", "")
+
+    # Extract tutorial number and tutorial name
+    if "_" in filename:
+        tutorial_num = filename.split("_")[0]
+        tutorial_name = filename.split("_")[1]
+
     # creating a chunks from the segments
+    chunks = []
+    for segment in result["segments"]:
+        chunks.append(
+            {
+                "tutorial_number": tutorial_num,
+                "tutorial_name": tutorial_name,
+                "start": segment["start"],
+                "end": segment["end"],
+                "duration": segment["end"] - segment["start"],
+                "text": segment["text"],
+            }
+        )
 
-    for segment in result['segments']:
-        chunks.append({"tutorial_number":tutorial_num,
-                    "tutorial_name":tutorial_name,
-                        "start": segment['start'],
-                        "end": segment['end'], 
-                        "text": segment['text']})
-        # chunk_with_metadata = {"chunks": chunks, "text" : result["text"]}
-    
+    chunk_with_metadata = {"chunks": chunks, "text": result["text"]}
 
+    #save using same filename
+    output_file = filename + "_chunks.json"
+    with open(f"data/chunks/{output_file}", "w", encoding="utf-8") as f:
+        json.dump(chunk_with_metadata, f, ensure_ascii=False, indent=2)
+    print(f"Saved {output_file} in {chunk_dir}")
 
-   
-
-    print(chunks)
-
-
-# #saving chunks in data/chunks
-# os.makedirs(chunk_dir, exist_ok=True)
-# with open(chunk_path, "w") as f:
-#     json.dump(chunk_with_metadata, f)
-
-# display_path = os.path.join(os.path.basename(base_dir), "data", "chunks", "chunks.json")
-# print(f"Your audio is converted into the texts succesfully!{display_path}")
+print("All transcripts converted into chunks succesfully!")
